5f369b009be1336c071f6c3597ed7ee9747fc1c7
Sameer0873 <mazeedansari786@gmail.com>
2018-08-20 22:04:00 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,80 @@
+1. Email for the short requirement estimation of having a range.
+
+Hello Aru,
+
+I went over the requirement and here is a general range for what you are looking at:
+
+PHP App: 130-180 Hrs
+1. User Registration & authentication.
+2. Creating/Editing todo list.
+3. Creating/Edding  todo items in each todo list
+4. Import a predefined todo list.
+
+Mobile App: 170-240 Hrs
+1. User Registration & authentication.
+2. Creating/Editing todo list.
+3. Creating/Edding  todo items in each todo list
+4. Import a predefined todo list.
+
+There are lots of assumptions here. We are assuming you need to create <All the assumption goes here>
+
+With the $9/hr rate, you can see the range of $100 - $120 for the PHP and 
+$500-$700 for mobile App. Something like this could be done in 2-3 Weeks in PHP and 4-6 weeks in Andriod.
+
+Let me know if you have any questions or if you want to pursue the idea further.
+===================================================================================================
+2. Email for the Long requirement estimation of having a sheet assocaited.
+
+Hi Aru,
+We have completed the estimation you have given us based on the documents. We only had 3 things to get confirmation on:
+1. 
+2. 
+3. 
+
+Ok if we assuming that <Assumtion goes here>
+
+If you have any questions about the estimate please let me know. Most likely we would resource 2 developers for this. So the sprint velocity for to each 2 Week cycle will be about 160 hours. We would see if we could ramp up faster and possibly get that closer to 180 hours.
+
+Design for this would depend completely on what you want it to look like. I would suggest building a UI kit and not an end to end design since the app could very easily be changed in the future. In this case, we would design a set of buttons, typography group and design elements that can easily be moved around/swapped out to create whatever page you needed. Like I described over the phone design hours billed at $50/hr.  Generally, a UI kit will take 10-30 hours depending on how much feedback we get. An end to end design would take a little bit more. I would need to see the list of sites that you like and would like to replicate first. If you need a logo designed first then I would add  $80-$100 (8-10 hours).
+==================================================================================================
+3. Quotations and Web, Mobile App Portfolio!
+
+Hi Alban, 
+
+Trust you're keeping good!!!
+
+As per discussion over Linkedin with the Yasir Sir.
+
+Please find the attachment as it contains Our Quotations and Website, Mobile App Development Portfolio.
+
+Also to let you know we have a team of the Sr. Professional Experts in-house in our company and working with the clients all around the globe.
+
+So, we are more than comfortable working on your project.
+
+Go Through it and update us so we can proceed accordingly.
+
+Awaiting for your kind response.
+==================================================================================================
+4. 
+
+Hi There,
+
+Trust you're keeping good!
+
+We have gone thru the post shared on Craigslist and according to it, sending you the attachments as it contains Our Company Profile, Web Developer Expert Resume, Web Development Portfolio Docs.
+
+Go thru it and let us know your feedback.
+
+Also to let you know we have a team of the Sr. Professional Experts in-house in our company and working with the clients all around the globe.
+
+So, we are more than comfortable working on your project.
+
+If you want to check the skills sets of our expert you can give them a test task. Also if be needed you can take an Interview, do let us know so we can schedule it accordingly.
+
+So, we can further proceed accordingly.
+
+Looking forward for a kind update.
+==================================================================================================
+
+
+
